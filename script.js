4ef72e75f57db25117663bdf753cc28ed9b09813
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-14 13:44:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -584,7 +584,7 @@ const bounds = [
 map.setMaxBounds(bounds);
 
 map.addControl(new mapboxgl.AttributionControl({
-  customAttribution: '<a href="https://omgeving.vlaanderen.be/nl/ena-economisch-netwerk-albertkanaal"></a><a href="https://vlaanderen.be">Vlaamse Overheid</a>'
+  customAttribution: '<a href="https://omgeving.vlaanderen.be/nl/ena-economisch-netwerk-albertkanaal">ENA</a> <a href="https://vlaanderen.be">Vlaamse Overheid</a>'
 }));
 
 class LogoVlaanderen {
